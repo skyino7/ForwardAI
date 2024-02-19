b3f2df8b48dade022d35b7170d0efb87273aacb7
Seth Ketsi <christambassador12@gmail.com>
2024-02-19 23:42:16 +0000

    Working on the Navbar Component and added the Styling

---
+++ frontend/src/Component/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Navbar.css';
+
+const navbar = () => {
+  return (
+    <div className='navbar'>
+        <p>Forward AI+</p>
+        <div className='nav-right'>
+            <a href='#'>Home</a>
+            <a href='#'>About</a>
+            <a href='#'>Features</a>
+            <a href='#'>Solutions</a>
+            <a href='#'>Contact</a>
+        </div>
+        <p className='end'>Sign Up</p>
+    </div>
+  )
+}
+
+export default navbar
