674c93d9f13a4ddfa51ab989e892a490e0c63003
Seth Ketsi <christambassador12@gmail.com>
2024-02-20 00:50:50 +0000

    Banner Component Added

--- frontend/src/Component/Banner.jsx
+++ frontend/src/Component/Banner.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import './Navbar.css';
+import './Comp.css';
+import dataImg from '../assets/data.png'
 
 const Banner = () => {
   return (
     <div className='banner'>
-        <h3>Forward AI+</h3>
-        <h1>DATA SPEAKS</h1>
-        <p>LET’S HELP YOU UNDERSTAND IT</p>
-        <button>Get Started</button>
+        <div className='data'>
+            <h3>Forward AI+</h3>
+            <h1>DATA SPEAKS</h1>
+            <p>LET’S HELP YOU UNDERSTAND IT</p>
+            <button>Get Started</button>
+        </div>
+        <div className="analytics">
+            <img src={dataImg} alt="Analytics" />
+        </div>
     </div>
   )
 }
