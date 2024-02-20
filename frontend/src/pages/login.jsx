64ed311b6488eda5387bcf2150db89e6192aaa44
Seth Ketsi <christambassador12@gmail.com>
2024-02-20 02:07:03 +0000

    WHAT WE OFFER component created

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../Component/Navbar'
 import Banner from '../Component/Banner'
+import Offer from '../Component/Offer'
 
 const login = () => {
   return (
     <>
         <Navbar />
         <Banner />
+        <Offer />
         <p className='login'>login</p>
     </>
   )
