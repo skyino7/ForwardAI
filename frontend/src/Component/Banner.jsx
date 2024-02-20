b56becd5921f0f515d8dd5a7f457d78eb421e4bf
Seth Ketsi <christambassador12@gmail.com>
2024-02-20 00:12:55 +0000

    updated

---
+++ frontend/src/Component/Banner.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Navbar.css';
+
+const Banner = () => {
+  return (
+    <div className='banner'>
+        <h3>Forward AI+</h3>
+        <h1>DATA SPEAKS</h1>
+        <p>LET’S HELP YOU UNDERSTAND IT</p>
+        <button>Get Started</button>
+    </div>
+  )
+}
+
+export default Banner
--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../Component/Navbar'
+import Banner from '../Component/Banner'
 
 const login = () => {
   return (
     <>
         <Navbar />
+        <Banner />
         <p className='login'>login</p>
     </>
   )
