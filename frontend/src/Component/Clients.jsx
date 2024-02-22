fcedd64ed126dbcf6e4b42cc2dc64340efcf1806
Seth Ketsi <christambassador12@gmail.com>
2024-02-22 01:10:50 +0000

    updated

--- frontend/src/Component/Clients.jsx
+++ frontend/src/Component/Clients.jsx
@@ -5,24 +5,45 @@ const Clients = () => {
   return (
     <div className='clients'>
         <h3>What Customers Say</h3>
-        <div className="choose-m">
-            <div className='column-choose'>
-                <img className='person' src={Person} alt="image" />
-                <h4>John Doe</h4>
-                <h3>pizzadoe, accra</h3>
-                <h5>Chief executive officer</h5>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </p>
+        <div className="clients-show">
+            <div className='column-client'>
+                <div className='content'>
+                    <img className='person' src={Person} alt="image" />
+                    <div className="text">
+                        <h4>John Doe</h4>
+                        <h3>pizzadoe, accra</h3>
+                        <h5>Chief executive officer</h5>
+                    </div>
+                </div>
+                <div className='description'>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
             </div>
-            <div className='column-choose'>
-                <h3>Tailored</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <div className='column-client'>
+                <div className='content'>
+                    <img className='person' src={Person} alt="image" />
+                    <div className="text">
+                        <h4>John Doe</h4>
+                        <h3>pizzadoe, accra</h3>
+                        <h5>Chief executive officer</h5>
+                    </div>
+                </div>
+                <div className='description'>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
             </div>
-            <div className='column-choose'>
-                <h3>Easy Data Integration</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <div className='column-client'>
+                <div className='content'>
+                    <img className='person' src={Person} alt="image" />
+                    <div className="text">
+                        <h4>John Doe</h4>
+                        <h3>pizzadoe, accra</h3>
+                        <h5>Chief executive officer</h5>
+                    </div>
+                </div>
+                <div className='description'>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
             </div>
         </div>
     </div>
