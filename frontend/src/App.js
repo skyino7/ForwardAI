345f8a80dd409167c9246e689405b37ecbc044aa
Seth Ketsi <christambassador12@gmail.com>
2024-02-23 23:54:52 +0000

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
-import Login from './pages/login';
+import Banner from './Component/Banner';
+import Navbar from './Component/Navbar';
+import Offer from './Component/Offer';
+import Choose from './Component/Choose';
+import Clients from './Component/Clients';
 
 function App() {
   return (
     <>
-      <Login />
+      <Navbar />
+      <Banner />
+      <Offer />
+      <Choose />
+      <Clients />
       {/* <h1>Forward AI+</h1> */}
     </>
   );
