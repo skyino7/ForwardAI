51af6cdb74abf3fe63fdcbc467bb0f2aefbbe47a
Seth Ketsi <christambassador12@gmail.com>
2024-02-25 21:15:20 +0000

    Login updated

--- front/src/pages/Login.jsx
+++ front/src/pages/Login.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
+import BI from '../assets/BI.jpg'
 
 const Login = () => {
   return (
-    <>
-      <h1>Login</h1>
-      <form className='login-form' action='post'>
-        <label htmlFor="username">Username</label>
-        <input type="text" placeholder="Username" />
-        <label htmlFor="password">Password</label>
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    </>
+    <div className='login'>
+      <div>
+        <h1>Login</h1>
+        <form className='login-form' action='post'>
+          <label htmlFor="username">Username</label>
+          <input type="text" placeholder="Username" />
+          <label htmlFor="password">Password</label>
+          <input type="password" placeholder="Password" />
+          <button type="submit">Login</button>
+        </form>
+      </div>
+      <div>
+        <img src={BI} alt="" />
+      </div>
+    </div>
   )
 }
 
