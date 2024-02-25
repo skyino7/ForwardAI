1892c07dd4be05b72fe81c519a16863a70b3045b
Seth Ketsi <christambassador12@gmail.com>
2024-02-25 21:39:37 +0000

    updated

--- front/src/pages/Login.jsx
+++ front/src/pages/Login.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import BI from '../assets/BI.jpg'
+import './main.css';
 
 const Login = () => {
   return (
     <div className='login'>
       <div>
-        <h1>Login</h1>
+        <h1>Forward AI+</h1>
+        <h3>Sign In</h3>
         <form className='login-form' action='post'>
-          <label htmlFor="username">Username</label>
-          <input type="text" placeholder="Username" />
+          <label htmlFor="username">Email</label>
+          <input type="text" placeholder="Your Email Address" />
           <label htmlFor="password">Password</label>
-          <input type="password" placeholder="Password" />
+          <input type="password" placeholder="Your Password" />
           <button type="submit">Login</button>
         </form>
       </div>
