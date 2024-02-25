a699e6ff5848a1a40f0a38490c649f93ee1e9218
Seth Ketsi <christambassador12@gmail.com>
2024-02-25 22:30:57 +0000

    updated

--- front/src/pages/Login.jsx
+++ front/src/pages/Login.jsx
@@ -5,21 +5,21 @@ import './main.css';
 const Login = () => {
   return (
     <div className='login'>
-      <div>
+      <div className='login-form'>
         <h1>Forward AI+</h1>
         <h3>Sign In</h3>
-        <form className='login-form' action='post'>
-          <label htmlFor="username">Email</label>
-          <input type="text" placeholder="Your Email Address" />
-          <label htmlFor="password">Password</label>
-          <input type="password" placeholder="Your Password" />
+        <form action='post'>
+          <label htmlFor="username">Email</label><br />
+          <input type="text" placeholder="Your Email Address" /><br />
+          <label htmlFor="password">Password</label><br />
+          <input type="password" placeholder="Your Password" /><br />
           <button type="submit">Login</button>
         </form>
       </div>
-      <div>
+      <div className='login-img'>
         <img src={BI} alt="" />
       </div>
-    </div>
+  </div>
   )
 }
 
