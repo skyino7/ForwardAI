19202df3f1a25f6e2916105960ef54ab25397bfc
Seth Ketsi <christambassador12@gmail.com>
2024-02-26 13:41:40 +0000

    updated

--- front/src/pages/Signup.jsx
+++ front/src/pages/Signup.jsx
@@ -6,14 +6,14 @@ const Signup = () => {
     <div className='signup'>
       <h1>Sign Up</h1>
       <form className='signup-form' action='post'>
-        <label htmlFor="name">Full Name</label>
-        <input type="text" name="name" id="name" />
-        <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" />
-        <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" />
-        <label htmlFor="confirm-password">Confirm Password</label>
-        <input type="password" name="confirm-password" id="confirm-password" />
+        <label htmlFor="name">Full Name</label> <br />
+        <input type="text" name="name" id="name" /> <br />
+        <label htmlFor="email">Email</label> <br />
+        <input type="email" name="email" id="email" /> <br />
+        <label htmlFor="password">Password</label> <br />
+        <input type="password" name="password" id="password" /> <br />
+        <label htmlFor="confirm-password">Confirm Password</label> <br />
+        <input type="password" name="confirm-password" id="confirm-password" /> <br />
         <button type="submit">Sign Up</button>
         <p>Already have an account? <Link to="/login">Sign in</Link></p>
       </form>
