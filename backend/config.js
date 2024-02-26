416af48ce3e307c890e04ed1987f075015a0e3e3
Seth Ketsi <christambassador12@gmail.com>
2024-02-26 17:45:45 +0000

    updated

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,52 @@
+const { config } = require('dotenv');
+const mysql = require('mysql2/promise');
+
+require('dotenv').config()
+
 module.exports = {
-    host: 'mysql_host',
-    user: 'mysql_user',
-    password: 'mysql_password',
-    database: 'database_name'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: 'forwardai'
 }
+
+async function checkDatabase(config) {
+
+    const conn = await mysql.createConnection(config);
+
+    try {
+        await conn.query(`SHOW DATABASES LIKE '${config.database}'`);
+        return true;
+    } catch (err) {
+        if (err.code === 'ER_DB_DOES_NOT_EXIST') {
+            return false;
+        } else {
+            throw err;
+        }
+    } finally {
+        conn.end();
+    }
+
+}
+
+async function createDatabase(config) {
+    const conn = await mysql.createConnection(config);
+
+    try {
+        await conn.query(`CREATE DATABASE IF NOT EXISTS ${config.database}`);
+        console.log(`Database '${config.database}' created successfully.`);
+    } catch (err) {
+        throw err;
+    } finally {
+        await conn.end();
+    }
+
+}
+
+(async () => {
+    if (!await checkDatabase(config)) {
+        await createDatabase(config)
+    } else {
+        console.log(`Database '${config.database}' already exists.`)
+    }
+})();
--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,12 @@ const express = require('express')
 const mysql = require('mysql2/promise')
 const config = require('./config')
 const bcrypt = require('bcryptjs')
+require('dotenv').config()
 
 const app = express()
-const port = 3000;
+const port = process.env.PORT || 4000;
 const pool = mysql.createPool(config)
 
-
 app.listen(port, () => {
-    console.log("Server is running on port ${port}");
+    console.log(`Server is running on port ${port}`);
 })
