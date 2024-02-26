963c022f73469afa2d21792211ef40eb7e41131c
Seth Ketsi <christambassador12@gmail.com>
2024-02-26 21:54:04 +0000

    user model created

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,28 @@ const app = express()
 const port = process.env.PORT || 4000;
 const pool = mysql.createPool(config)
 
+app.post('/signup', async (req, res) => {
+    try {
+        const { name, email, password, confirmPassword } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const [rows] = await pool.execute(
+            'INSERT INTO users (name, email, password, confirmPassword) VALUES (?, ?, ?, ?)',
+            [name, email, hashedPassword, confirmPassword]
+        );
+
+        if (rows.affectedRows > 0) {
+            res.status(201).json({ message: 'User Created Successfully' });
+        } else {
+            res.status(400).json({ message: 'Failed to Create User' });
+        }
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Registration failed' });
+    }
+})
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
