a854550978211d9b49daad991b4a912a4f21958c
Seth Ketsi <christambassador12@gmail.com>
2024-02-28 00:41:55 +0000

    updated

--- front/src/Component/Started.jsx
+++ front/src/Component/Started.jsx
@@ -6,17 +6,22 @@ import { Link } from 'react-router-dom';
 const Started = () => {
   return (
     <div className='started'>
-        <h2>GET HELP WITH OUR BUSINESS INTELLIGENCE
-        <br />AND ANALYTICS TOOL </h2>
+        <div className='text-start'>
+            <h2>GET HELP WITH OUR BUSINESS INTELLIGENCE
+            <br />AND ANALYTICS TOOL </h2>
 
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat.</p>
 
-        <p><Link to="/signup">Get Started</Link></p>
+            <p><Link to="/signup">Get Started</Link></p>
+        </div>
 
-        <img src={Start} alt="" height={400}/>
+        <div className="start-img">
+            <img src={Start} alt="" height={400}/>
+        </div>
+
     </div>
   )
 }
