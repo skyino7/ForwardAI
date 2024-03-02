af84e27bfa4093c5f0da4753ef6e58f74aca271b
Seth Ketsi <christambassador12@gmail.com>
2024-03-02 20:06:58 +0000

    updated - working now

--- front/src/pages/Login.jsx
+++ front/src/pages/Login.jsx
@@ -26,7 +26,7 @@ const Login = () => {
     event.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:4000/login',{
+      const response = await fetch('/login',{
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
@@ -38,9 +38,9 @@ const Login = () => {
       if (response.ok){
         setRedirect(true);
         setMessage('Login Successful');
-        navigate('/Dashboard')
+        navigate('/Dashboard');
       } else {
-        setMessage('Login Failed')
+        setMessage('Login Failed');
       }
     } catch (err) {
       console.error('Error:', err)
--- front/src/pages/Signup.jsx
+++ front/src/pages/Signup.jsx
@@ -20,7 +20,7 @@ const Signup = () => {
     event.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:4000/signup', {
+      const response = await fetch('http://localhost:5000/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
